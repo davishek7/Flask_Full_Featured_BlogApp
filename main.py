d2a737082bbbcb7611f3538d73bd5ac247fe9bb5
Avishek <davishek7@gmail.com>
2020-10-28 23:30:05 +0530

    Flask Blog Commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,url_for
+from forms import RegistrationForm,LoginForm
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='a1e824f498ef42682a1e81bce53cbd2db3'
+
+posts=[
+    {
+        'author':'avishek',
+        'title':'First Post',
+        'content':'First Content',
+        'date_posted':'October 28,2020'
+    },
+    {
+        'author':'bubai',
+        'title':'Second Post',
+        'content':'Second Content',
+        'date_posted':'October 29,2020'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('home.html',posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title="About")
+
+@app.route('/register')
+def register():
+    form=RegistrationForm()
+    return render_template('register.html',title="Register",form=form)
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html',title="Login",form=form)
+
+if __name__=='__main__':
+    app.run(debug=True)
