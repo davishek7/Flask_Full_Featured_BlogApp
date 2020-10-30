96fa88d1bbc85e1b5c59b2982f1e8dc9e6fe816c
Avishek <davishek7@gmail.com>
2020-10-30 14:48:32 +0530

    30th October 2020 Commit

--- flaskblogapp/models.py
+++ flaskblogapp/models.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from flaskblogapp import db
+from flaskblogapp import db,login_manager
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model,UserMixin):
 
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(10),unique=True,nullable=False)
