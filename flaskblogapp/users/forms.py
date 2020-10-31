82898ec05d090e8f5da6c434fd363072d1242300
Avishek <davishek7@gmail.com>
2020-11-01 00:10:27 +0530

    Blueprint,Custom error pages added

--- flaskblogapp/users/forms.py
+++ flaskblogapp/users/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileField,FileAllowed
+from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField
+from wtforms.validators import DataRequired,ValidationError,Length,Email,EqualTo
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError,TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flaskblogapp.models import User
-
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
@@ -66,11 +65,6 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is already registered')
-
-class PostForm(FlaskForm):
-    title=StringField('Title',validators=[DataRequired()])
-    content=TextAreaField('Content',validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flaskblogapp import app
+from flaskblogapp import create_app
+
+app=create_app()
 
 if __name__=='__main__':
     
