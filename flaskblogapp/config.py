6da46029d6f70b3253e49043647f69e48d7d9302
Avishek <davishek7@gmail.com>
2020-11-01 12:10:25 +0530

    Initial deploy

--- flaskblogapp/config.py
+++ flaskblogapp/config.py
@@ -1,10 +1,19 @@
 import os
+
+
 class Config:
-    SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-    MAIL_SERVER='smtp.gmail.com'
-    MAIL_PORT=465
-    MAIL_USE_SSL=True
-    MAIL_USERNAME=os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD=os.environ.get('EMAIL_PASS')
+
+    env = 'prod'
+
+    if env == 'dev':
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    else:
+        SQLALCHEMY_DATABASE_URI = 'postgres://unqmjvhjpmzquq:9531e778bb5abc2fa677335ae1445b6de1ace8fd0c2be1bbd0e7fc26d202a24e@ec2-3-208-224-152.compute-1.amazonaws.com:5432/d6854u8oh6u03g'
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
