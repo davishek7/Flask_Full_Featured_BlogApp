9f6480ddebc2224fe7917767414c993a479e8c66
Avishek <davishek7@gmail.com>
2020-11-01 15:00:59 +0530

    Initial deploy

--- flaskblogapp/__init__.py
+++ flaskblogapp/__init__.py
@@ -16,7 +16,8 @@ def create_app(config_class=Config):
     app=Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)
+    with app.app_context():
+        db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
