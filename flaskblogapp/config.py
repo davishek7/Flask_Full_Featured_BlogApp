7e2b859e933baa2e3ee716aece594c913b82518e
Avishek <davishek7@gmail.com>
2020-12-05 16:10:48 +0530

    Some fixes

--- flaskblogapp/config.py
+++ flaskblogapp/config.py
@@ -3,8 +3,9 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    MAIL_SERVER='smtp.gmail.com'
-    MAIL_PORT=465
-    MAIL_USE_SSL=True
+    MAIL_SERVER='smtp.mailtrap.io'
+    MAIL_PORT=2525
     MAIL_USERNAME=os.environ.get('EMAIL_USER')
     MAIL_PASSWORD=os.environ.get('EMAIL_PASS')
+    MAIL_USE_TLS=True
+    MAIL_USE_SSL=False
